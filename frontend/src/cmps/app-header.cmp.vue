27fc75631ff735ad87a110fc57fbf975bd30dc9b
Idanatiya <idanatiya122@gmail.com>
2020-11-27 17:58:11 +0200

    nice tab

--- frontend/src/cmps/app-header.cmp.vue
+++ frontend/src/cmps/app-header.cmp.vue
@@ -4,27 +4,23 @@
             <!-- <img src="@/assets/img/logo1.png" alt="" class="app-logo"> -->
             Logo
         </div>
-        <div class="nav-links">
+        <div class="nav-links flex align-center">
             <router-link to="/">Home</router-link>
             <router-link to="/wap">templates</router-link>
             <router-link to="/editor">Editor</router-link>
             <router-link to="/playground">Playground</router-link>
+            <avatar :username="'matan cohen'" :size="45" :color="'#03A9F4'" />
+
         </div>
     </section>
 </template>
 
 <script>
-import avatar from '@/cmps/avatar.vue';
+import avatar from '@/cmps/avatar.cmp.vue';
 export default {
     name: "app-header",
-    data() {
-        return {};
-    },
     components: {
         avatar
     }
 };
 </script>
-
-<style>
-</style>
---
+++ frontend/src/cmps/custum-cmps/avatar-modal.cmp.vue
@@ -0,0 +1,20 @@
+<template>
+    <section class="avatar-modal flex column">
+        <avatar :username="'matan cohen'" :size="80" :color="'#03A9F4'" />
+        <p>Name</p>
+        <small>email</small>
+    </section>
+</template>
+
+<script>
+import avatar from '@/cmps/avatar.cmp.vue';
+export default {
+    name: 'avatar-modal',
+    components: {
+        avatar
+    }
+}
+</script>
+
+<style>
+</style>
