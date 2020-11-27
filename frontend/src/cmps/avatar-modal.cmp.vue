480d16ced202b57a9d65fbfcd833df450ac39f91
Idanatiya <idanatiya122@gmail.com>
2020-11-27 19:00:35 +0200

    added user modal

---
+++ frontend/src/cmps/avatar-modal.cmp.vue
@@ -0,0 +1,27 @@
+<template>
+    <section class="avatar-modal flex column align-center">
+        <div class="info flex column align-center">
+            <avatar :username="'matan cohen'" :size="80" :color="'#03A9F4'" />
+            <h3>Matan Cohen</h3>
+            <span>emailgaming@gmail.com</span>
+        </div>
+        <div class="seperator"></div>
+        <section class="modal-links flex column ">
+            <span><i class="far fa-user"></i> Login</span>
+            <span><i class="fas fa-sign-out-alt"></i> Sign up</span>
+        </section>
+    </section>
+</template>
+
+<script>
+import avatar from '@/cmps/avatar.cmp.vue';
+export default {
+    name: 'avatar-modal',
+    components: {
+        avatar
+    }
+}
+</script>
+
+<style>
+</style>
