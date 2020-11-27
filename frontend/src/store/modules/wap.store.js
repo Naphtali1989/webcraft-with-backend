2f7644be8703ae9e7af04809549a4aea2ab087e5
Matan Cohen <matanco3605@gmail.com>
2020-11-27 03:23:25 +0200

    changes

--- frontend/src/store/modules/wap.store.js
+++ frontend/src/store/modules/wap.store.js
@@ -100,6 +100,153 @@ export const wapStore = {
                         ]
                     }
                 ]
+            },
+            {
+                id: Math.random().toString(36).substring(2, 10),
+                name: "section",
+                type: "cards",
+                class: "",
+                style: {
+                    minHeight: "872px",
+                    padding: "",
+                    background: "beige",
+                    width: "100%",
+                    borderRadius: 0
+                },
+                children: [{
+                    id: Math.random().toString(36).substring(2, 10),
+                    name: "section",
+                    class: "hero flex wrap",
+                    style: {
+                        lineHeight: "1.5",
+                        backgroundColor: "",
+                        color: 'black',
+                        flex: "1 1 20rem",
+                        fontFamily: "Ruhl",
+                        fontSize: "16px",
+                        textAlign: '',
+                        fontStyle: '',
+                        textDecoration: '',
+                        letterSpacing: '',
+                        width: "95%",
+                        flexWrap: "wrap",
+                        margin: "0 0 0 auto",
+                        overflow: "hidden"
+                    },
+                    children: [{
+                            id: Math.random().toString(36).substring(2, 10),
+                            name: "div",
+                            class: "hero-introduction",
+                            style: {
+                                background: "",
+                                flex: "2 1 40rem",
+                                textAlign: "center",
+                                borderRadius: 0,
+                            },
+                            children: [{
+                                    id: Math.random().toString(36).substring(2, 10),
+                                    type: "text",
+                                    name: "txt",
+                                    txt: "Elena Joy",
+                                    style: {
+                                        fontSize: "1.5rem",
+                                        lineHeight: "1.5",
+                                        letterSpacing: "",
+                                        background: "",
+                                        color: "rgb(63,63,63)",
+                                        fontFamily: "Ruhl",
+                                        textDecoration: "",
+                                        fontStyle: "",
+                                        textAlign: "",
+                                        paddingTop: "3rem",
+                                    }
+                                }, {
+                                    id: Math.random().toString(36).substring(2, 10),
+                                    type: "text",
+                                    name: "txt",
+                                    txt: "Photography",
+                                    style: {
+                                        fontSize: "1.5rem",
+                                        lineHeight: "1.5",
+                                        letterSpacing: "",
+                                        background: "",
+                                        color: "rgb(63,63,63)",
+                                        fontFamily: "Ruhl",
+                                        textDecoration: "",
+                                        fontStyle: "",
+                                        textAlign: "",
+                                        paddingTop: "3rem",
+                                    }
+                                },
+                                {
+                                    id: Math.random().toString(36).substring(2, 10),
+                                    type: "text",
+                                    name: "txt",
+                                    class: "",
+                                    txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque minima assumenda expedita similique quaerat maiores ab, itaque hic explicabo eaque!",
+                                    style: {
+                                        lineHeight: "",
+                                        backgroundColor: "",
+                                        color: 'rgb(63,63,63)',
+                                        fontFamily: "Ruhl",
+                                        fontSize: "1rem",
+                                        textAlign: '',
+                                        fontStyle: '',
+                                        textDecoration: '',
+                                        letterSpacing: '',
+                                        padding: "5rem 10rem"
+                                    }
+                                }, {
+                                    id: Math.random().toString(36).substring(2, 10),
+                                    type: "link",
+                                    name: "link",
+                                    class: "",
+                                    txt: "Gallery",
+                                    href: "#Gallery",
+                                    style: {
+                                        lineHeight: "",
+                                        backgroundColor: "",
+                                        color: 'rgb(63,63,63)',
+                                        fontFamily: "Ruhl",
+                                        fontSize: "1rem",
+                                        textAlign: '',
+                                        fontStyle: '',
+                                        textDecoration: '',
+                                        letterSpacing: '',
+                                        padding: "2rem 8rem",
+                                        border: "3px solid black",
+                                        marginBottom: "4rem"
+                                    }
+                                }
+                            ]
+                        },
+                        {
+                            id: Math.random().toString(36).substring(2, 10),
+                            name: "div",
+                            class: "hero-images",
+                            style: {
+                                background: "linear-gradient(#dad8dd, #dad1d4)",
+                                flex: "1 1 40rem",
+                                borderRadius: 0,
+                                zIndex: 1,
+                            },
+                            children: [{
+                                id: Math.random().toString(36).substring(2, 10),
+                                name: "img",
+                                class: "hero-elena",
+                                style: {
+                                    borderRadius: "2px",
+                                    height: "872px",
+                                    width: "100%",
+                                    objectFit: "cover"
+                                },
+                                imgUrl: "@/assets/img/elenajoy.png"
+                            }]
+                        }
+
+
+                    ]
+                }]
             }
 
         ]
