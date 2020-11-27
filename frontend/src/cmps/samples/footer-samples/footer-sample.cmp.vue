774733a0f9648a72f053d08626335e6dc755f5ff
Matan Cohen <matanco3605@gmail.com>
2020-11-27 23:20:56 +0200

    suka

--- frontend/src/cmps/samples/footer-samples/footer-sample.cmp.vue
+++ frontend/src/cmps/samples/footer-samples/footer-sample.cmp.vue
@@ -1,15 +1,25 @@
 <template>
-    <div class="footer">
-        <h4>Elena Joy </h4>
-        <ul>
-             <li>
-                <a href="#"><img src="@/assets/svgs/youtube-symbol1.svg" alt="youtube-social-media" /></a>
+    <div class="footer flex wrap align-center">
+        <h4>Elena Joy</h4>
+        <ul class="clean-list flex space-between align-center">
+            <li>
+                <a href="#"
+                    ><img
+                        src="@/assets/svgs/youtube-symbol1.svg"
+                        alt="youtube-social-media"
+                /></a>
             </li>
             <li>
-                <a href="#"><img src="@/assets/svgs/twitter1.svg" alt="twitter-social-media" /></a>
+                <a href="#"
+                    ><img
+                        src="@/assets/svgs/twitter1.svg"
+                        alt="twitter-social-media"
+                /></a>
             </li>
             <li>
-                <a href="#"><img src="@/assets/svgs/instagram1.svg" alt="instagram" /></a>
+                <a href="#"
+                    ><img src="@/assets/svgs/instagram1.svg" alt="instagram"
+                /></a>
             </li>
         </ul>
     </div>
@@ -18,6 +28,76 @@
 <script>
 export default {
     name: "footer-sample",
+    data() {
+        return {
+            id: Math.random().toString(36).substring(2, 10),
+            type: "footer",
+            thumbnail: "https://imagizer.imageshack.com/img922/9372/4iyvDo.png",
+            title: "Footer",
+            name: "section",
+            class: "footer flex wrap align-center",
+            style: {
+                padding: "1rem 5%",
+                color: "white",
+                background: "lightblue",
+                backgroundColor: "lightblue",
+                borderRadius: 0,
+            },
+            children: [{
+                id: Math.random().toString(36).substring(2, 10),
+                name: "txt",
+                class: "logo",
+                txt: "Elena Joy",
+                style: {
+                    lineHeight: "1.5",
+                    backgroundColor: "transparent",
+                    color: 'white',
+                    fontFamily: "Ruhal",
+                    fontSize: "30px",
+                    textAlign: 'inherit',
+                    fontStyle: 'normal',
+                    textDecoration: 'none',
+                    letterSpacing: '1',
+                    flex: "3 1 40rem",
+                    marginInlineEnd: "40rem"
+
+                }
+            }, {
+                id: Math.random().toString(36).substring(2, 10),
+                name: 'div',
+                class: "clean-list flex space-between align-center",
+                style: {
+                    background: "transparent",
+                    flex: "1 1 6rem",
+                    borderRadius: 0,
+                },
+                children: [{
+                    id: Math.random().toString(36).substring(2, 10),
+                    name: 'link',
+                    style:{
+                        background:"url(@/assets/svgs/youtube-symbol1.svg)"
+                    }
+                },{
+                    id: Math.random().toString(36).substring(2, 10),
+                    name: 'link',
+                    style:{
+                        background:"url(@/assets/svgs/twitter1.svg)"
+                    }
+                },{
+                    id: Math.random().toString(36).substring(2, 10),
+                    name: 'link',
+                    style:{
+                        background:"url(@/assets/svgs/instagram1.svg)"
+                    }
+                }
+                ]
+            }
+            ]
+
+        }
+
+
+    }
 }
 </script>
 
@@ -25,20 +105,15 @@ export default {
 .footer {
     background-color: lightblue;
     color: white;
-    display: flex;
-    padding: 3rem 5%;
-    align-items: center;
-    flex-wrap: wrap;
+    padding: 1rem 5%;
 
     ul {
-        display: flex;
-        flex: 1 1 40rem;
-        justify-content: space-between;
-        align-items: center;
-    }
-    h4 {
-        flex: 3 1 40rem;
-        font-size: calc(1.4rem + 1vw);
+        flex: 1 1 6rem;
+        h4 {
+            flex: 3 1 40rem;
+            font-size: calc(1.4rem + 1vw);
+            margin-inline-end: 40rem;
+        }
     }
 }
 </style>
