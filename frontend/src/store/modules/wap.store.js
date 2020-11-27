3201625c8e027d21b9a7a59570e16fdbcad95e67
Idanatiya <idanatiya122@gmail.com>
2020-11-27 23:19:41 +0200

    a

--- frontend/src/store/modules/wap.store.js
+++ frontend/src/store/modules/wap.store.js
@@ -256,6 +256,123 @@ export const wapStore = {
 
                     ]
                 }]
+            },
+            {
+                id: Math.random().toString(36).substring(2, 10),
+                name: "section",
+                type: "cards",
+                class: "card-section flex",
+                thumbnail: "https://imagizer.imageshack.com/img922/1905/up2tR2.png",
+                style: {
+                    background: "transparent",
+                    borderRadius: "0px",
+                    height: "400px",
+                },
+                children: [{
+                        id: Math.random().toString(36).substring(2, 10),
+                        name: "div",
+                        type: "card-div",
+                        class: "card flex column align-center text-center",
+                        style: {
+                            borderRadius: '0px',
+                            background: "#69D2E7",
+                            width: '300px',
+                            height: "380px",
+                            position: 'relative'
+                        },
+                        //header
+                        children: [{
+                            id: Math.random().toString(36).substring(2, 10),
+                            name: 'div',
+                            type: 'header-div',
+                            style: {
+                                height: '120px',
+                                width: '100%',
+                                background: 'url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEPu7BceUzLF-DvrMQdcldR3W_5Gn9BhKsiw&usqp=CAU)'
+                            },
+                            children: [{
+                                id: Math.random().toString(36).substring(2, 10),
+                                name: 'div',
+                                class: 'avatar-div',
+                                style: {
+                                    width: "80px",
+                                    height: "80px",
+                                    position: "relative",
+                                    margin: "0 auto"
+                                },
+                                children: [{
+                                    id: Math.random().toString(36).substring(2, 10),
+                                    name: 'img',
+                                    class: 'card-img',
+                                    imgUrl: 'https://yt3.ggpht.com/ytc/AAUvwnjr0yv7VKDZVgTVKU28P9ApPgt8nBaQwZjc78OOiw=s900-c-k-c0x00ffffff-no-rj',
+                                    style: {
+                                        display: 'block',
+                                        position: 'relative',
+                                        bottom: '-70px',
+                                        border: "4px solid white",
+                                        objectFit: 'cover',
+                                        height: '100%',
+                                        borderRadius: '50%'
+                                    }
+                                }]
+                            }]
+                        }],
+
+                    },
+                    {
+                        id: Math.random().toString(36).substring(2, 10),
+                        name: 'div',
+                        type: 'card-div',
+                        class: 'card-body flex column align-center text-center',
+                        style: {
+                            padding: '30px',
+                            position: 'absolute'
+                        },
+                        children: [{
+                                id: Math.random().toString(36).substring(2, 10),
+                                name: 'txt',
+                                txt: "place holder",
+                                style: {
+                                    paddingTop: '20px',
+                                    fontSize: '18px',
+                                    fontweight: '600'
+                                }
+                            },
+                            {
+                                id: Math.random().toString(36).substring(2, 10),
+                                name: 'txt',
+                                txt: "place holder",
+                                style: {
+                                    paddingTop: '20px',
+                                    fontSize: '25px',
+                                    color: '#7f00ff'
+                                }
+                            },
+                            {
+                                id: Math.random().toString(36).substring(2, 10),
+                                name: 'txt',
+                                txt: 'place holder',
+                                style: {
+                                    paddingTop: '8px',
+                                    fontSize: '25px'
+                                }
+                            },
+                            {
+                                id: Math.random().toString(36).substring(2, 10),
+                                name: 'link',
+                                txt: 'place holder',
+                                style: {
+                                    padding: "16px 20px",
+                                    background: '#7f00ff',
+                                    borderColor: '#7f00ff',
+                                    color: 'white',
+                                    borderRadius: '100px'
+                                }
+                            }
+                        ]
+
+                    }
+                ]
             }
 
         ]
