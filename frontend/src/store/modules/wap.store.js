a5837eb82c98a030f9e7d223b8571604e72fd5cb
Matan Cohen <matanco3605@gmail.com>
2020-11-28 17:18:56 +0200

    placeholder dnd

--- frontend/src/store/modules/wap.store.js
+++ frontend/src/store/modules/wap.store.js
@@ -767,95 +767,95 @@ export const wapStore = {
                 ]
             },
             //next one 
-            // {
-            //     id: Math.random().toString(36).substring(2, 10),
-            //     type: "footer",
-            //     thumbnail: "https://imagizer.imageshack.com/img922/9372/4iyvDo.png",
-            //     title: "Footer",
-            //     name: "section",
-            //     class: "footer flex wrap align-center",
-            //     style: {
-            //         padding: "1rem 5%",
-            //         color: "white",
-            //         background: "lightblue",
-            //         backgroundColor: "lightblue",
-            //         borderRadius: 0,
-            //     },
-            //     children: [{
-            //         id: Math.random().toString(36).substring(2, 10),
-            //         name: "txt",
-            //         class: "logo",
-            //         txt: "Elena Joy",
-            //         style: {
-            //             lineHeight: "1.5",
-            //             backgroundColor: "transparent",
-            //             color: 'white',
-            //             fontFamily: "Ruhal",
-            //             fontSize: "30px",
-            //             textAlign: 'inherit',
-            //             fontStyle: 'normal',
-            //             textDecoration: 'none',
-            //             letterSpacing: '1',
-            //             flex: "3 1 40rem",
-            //         }
-            //     }, {
-            //         id: Math.random().toString(36).substring(2, 10),
-            //         name: 'div',
-            //         class: "clean-list flex space-between align-center",
-            //         style: {
-            //             background: "transparent",
-            //             flex: "1 1 6rem",
-            //             borderRadius: 0,
-            //         },
-            //         children: [{
-            //             id: Math.random().toString(36).substring(2, 10),
-            //             name: 'link',
-            //             style: {
-            //                 height: "50px"
-            //             },
-            //             children: [{
-            //                 id: Math.random().toString(36).substring(2, 10),
-            //                 name: "i",
-            //                 class: "fab fa-twitter",
-            //                 style: {
-            //                     fontSize: "36px",
-            //                     lineHeight: "1.5"
-            //                 },
-            //             }]
-            //         }, {
-            //             id: Math.random().toString(36).substring(2, 10),
-            //             name: 'link',
-            //             style: {
-            //                 height: "50px"
-            //             },
-            //             children: [{
-            //                 id: Math.random().toString(36).substring(2, 10),
-            //                 name: "i",
-            //                 class: "fab fa-instagram",
-            //                 style: {
-            //                     fontSize: "36px",
-            //                     lineHeight: "1.5"
-            //                 },
-            //             }]
-            //         }, {
-            //             id: Math.random().toString(36).substring(2, 10),
-            //             name: 'link',
-            //             style: {
-            //                 height: "50px"
-            //             },
-            //             children: [{
-            //                 id: Math.random().toString(36).substring(2, 10),
-            //                 name: "i",
-            //                 class: "fab fa-facebook",
-            //                 style: {
-            //                     fontSize: "36px",
-            //                     lineHeight: "1.5"
-            //                 },
-            //             }]
-            //         }]
-            //     }]
+            {
+                id: Math.random().toString(36).substring(2, 10),
+                type: "footer",
+                thumbnail: "https://imagizer.imageshack.com/img922/9372/4iyvDo.png",
+                title: "Footer",
+                name: "section",
+                class: "footer flex wrap align-center",
+                style: {
+                    padding: "1rem 5%",
+                    color: "white",
+                    background: "lightblue",
+                    backgroundColor: "lightblue",
+                    borderRadius: 0,
+                },
+                children: [{
+                    id: Math.random().toString(36).substring(2, 10),
+                    name: "txt",
+                    class: "logo",
+                    txt: "Elena Joy",
+                    style: {
+                        lineHeight: "1.5",
+                        backgroundColor: "transparent",
+                        color: 'white',
+                        fontFamily: "Ruhal",
+                        fontSize: "30px",
+                        textAlign: 'inherit',
+                        fontStyle: 'normal',
+                        textDecoration: 'none',
+                        letterSpacing: '1',
+                        flex: "3 1 40rem",
+                    }
+                }, {
+                    id: Math.random().toString(36).substring(2, 10),
+                    name: 'div',
+                    class: "clean-list flex space-between align-center",
+                    style: {
+                        background: "transparent",
+                        flex: "1 1 6rem",
+                        borderRadius: 0,
+                    },
+                    children: [{
+                        id: Math.random().toString(36).substring(2, 10),
+                        name: 'link',
+                        style: {
+                            height: "50px"
+                        },
+                        children: [{
+                            id: Math.random().toString(36).substring(2, 10),
+                            name: "i",
+                            class: "fab fa-twitter",
+                            style: {
+                                fontSize: "36px",
+                                lineHeight: "1.5"
+                            },
+                        }]
+                    }, {
+                        id: Math.random().toString(36).substring(2, 10),
+                        name: 'link',
+                        style: {
+                            height: "50px"
+                        },
+                        children: [{
+                            id: Math.random().toString(36).substring(2, 10),
+                            name: "i",
+                            class: "fab fa-instagram",
+                            style: {
+                                fontSize: "36px",
+                                lineHeight: "1.5"
+                            },
+                        }]
+                    }, {
+                        id: Math.random().toString(36).substring(2, 10),
+                        name: 'link',
+                        style: {
+                            height: "50px"
+                        },
+                        children: [{
+                            id: Math.random().toString(36).substring(2, 10),
+                            name: "i",
+                            class: "fab fa-facebook",
+                            style: {
+                                fontSize: "36px",
+                                lineHeight: "1.5"
+                            },
+                        }]
+                    }]
+                }]
 
-            // },
+            },
             // Next!
             {
                 "id": Math.random().toString(36).substring(2, 10),
