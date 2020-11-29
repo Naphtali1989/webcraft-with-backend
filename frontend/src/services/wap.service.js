ae20055fa448546665b134c56f2ebc64e65118ac
Naphtali Rubin <naphtali.r.89@gmail.com>
2020-11-29 11:29:59 +0200

    pilpesh

--- frontend/src/services/wap.service.js
+++ frontend/src/services/wap.service.js
@@ -59,15 +59,15 @@ export const wapService = {
 // function _createNode(data) {
 //     const { localName, style, className, attributes } = data;
 //     const node = {
-//         id: Math.random().toString(36).slice(2),
-//         el: localName,
+//         _id: Math.random().toString(36).slice(2),
+//         name: localName,
 //         style: style.cssText,
 //         class: className,
 //         children: []
 //     };
-//     if (node.class === 'vue-map-container') node.el = 'google-map';
-//     if (node.el === 'img') node.imgUrl = attributes.src.value;
-//     if (node.el === 'iframe') node.vidUrl = attributes.src.value;
+//     if (node.class === 'vue-map-container') node.name = 'google-map';
+//     if (node.name === 'img') node.imgUrl = attributes.src.value;
+//     if (node.name === 'iframe') node.vidUrl = attributes.src.value;
 //     if (attributes.type) node.type = attributes.type.value;
 //     if (attributes.href) node.href = attributes.href.value;
 //     return node;
