859f2025c182818565e75f9d3445f840f0c80506
Matan Cohen <matanco3605@gmail.com>
2020-11-30 21:06:25 +0200

    started wap gallery

--- frontend/src/services/editor.service.js
+++ frontend/src/services/editor.service.js
@@ -1,1 +1,10 @@
-export const editorService = {}
+import httpService from './http.service.js';
+
+function query() {
+    const samples = httpService.get('sample');
+    return samples;
+}
+
+export const editorService = {
+    query
+}
