258e7cad96eb9e5ea5f651f413587eeaf01021f7
Idanatiya <idanatiya122@gmail.com>
2020-12-01 17:24:24 +0200

    a

--- frontend/src/views/home.cmp.vue
+++ frontend/src/views/home.cmp.vue
@@ -1,8 +1,17 @@
 <template>
     <section class="content-wrapper">
-        <div class="main-hero">
-            <h1>Get your peresonal website with a few clicks!</h1>
-
+        <div class="intro">
+            <h1>Build a website easily!</h1>
+            <img class="img1" src="https://imagizer.imageshack.com/img924/8105/aLPK9v.png" />
+        </div>
+        <div class="intro">
+            <h1>Build a website easily!</h1>
+            <img class="img2" src="https://imagizer.imageshack.com/img922/6240/9IpCzn.png" />
+        </div>
+        <div class="intro">
+            <h1 class="title">Just Drag Drop</h1>
+            <p class="intro-text">With a super most innovative drag and drop website builder, you can design any website you want. Just Drag, Drop and Customize!
+                All the templates below were built using Webcraft Platform.</p>
         </div>
 
     </section>
