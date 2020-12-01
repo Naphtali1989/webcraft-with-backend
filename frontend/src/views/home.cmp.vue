6c147caec2e2e54eba3474f3576d958fd3ff1bea
Naphtali Rubin <naphtali.r.89@gmail.com>
2020-12-01 21:34:44 +0200

    started working on home page

--- frontend/src/views/home.cmp.vue
+++ frontend/src/views/home.cmp.vue
@@ -1,15 +1,16 @@
 <template>
-    <section class="content-wrapper">
-        <div class="intro">
+    <section class="content-wrapper main-layout">
+        <div class="main-hero flex">
             <h1>Build a website easily!</h1>
-            <img class="img1" src="https://imagizer.imageshack.com/img924/8105/aLPK9v.png" />
+           
         </div>
         <div class="intro">
             <h1>Build a website easily!</h1>
-            <img class="img2" src="https://imagizer.imageshack.com/img922/6240/9IpCzn.png" />
+             <img class="img1" src="https://imagizer.imageshack.com/img924/8105/aLPK9v.png" />
         </div>
         <div class="intro">
             <h1 class="title">Just Drag Drop</h1>
+            <img class="img2" src="https://imagizer.imageshack.com/img922/6240/9IpCzn.png" />
             <p class="intro-text">With a super most innovative drag and drop website builder, you can design any website you want. Just Drag, Drop and Customize!
                 All the templates below were built using Webcraft Platform.</p>
         </div>
@@ -20,7 +21,7 @@
 
 <script>
 
-import loader from '../cmps/custum-cmps/loader.cmp';
+import loader from '@/cmps/custum-cmps/loader.cmp';
 export default {
     name: 'home',
     components: {
