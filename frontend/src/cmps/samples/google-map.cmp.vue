68da7cfac877c3968d611431fda6c327d3bf0d2e
Idanatiya <idanatiya122@gmail.com>
2020-12-02 02:47:45 +0200

    fix map info

--- frontend/src/cmps/samples/google-map.cmp.vue
+++ frontend/src/cmps/samples/google-map.cmp.vue
@@ -1,42 +1,38 @@
 <template>
 
-    <GmapMap
-        :center="{ lat: 32.088028, lng: 34.803222 }"
-        :zoom="15"
-        map-type-id="terrain"        
-    >
-        <GmapMarker
-            :key="index"
-            v-for="(m, index) in markers"
-            :position="m.position"
-            :clickable="true"
-            :draggable="true"
-            @click="center = m.position"
-        />
+    <GmapMap :center="getCoords" :zoom="getZoom" map-type-id="terrain">
+        <GmapMarker :key="index" v-for="(m, index) in markers" :position="m.position" :clickable="true" :draggable="true" @click="center = m.position" />
     </GmapMap>
 </template>
 
 <script>
 export default {
-    name:'google-map',
+    name: 'google-map',
     props: {
-        position: Object,
-        lockIdx:Number
+        info: Object,
     },
     data() {
         return {
             markers: [
-                { position: { lat: 32.088028, lng: 34.803222 } },
+                { position: { lat: 32.088028,lng: 34.803222 } },
 
             ]
         }
     },
-    methods:{
+    methods: {
     },
-    computed:{
-        
-    }
-  
+    computed: {
+        getCoords() {
+            return {
+                lat: this.info.position.lat,
+                lng: this.info.position.lng,
+            }
+        },
+        getZoom() {
+            return this.info.zoom;
+        }
+    },
+
 }
 </script>
 
