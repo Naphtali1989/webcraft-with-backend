503805e107ae4ca5428e66f4e17120b4c1b7c718
Naphtali Rubin <naphtali.r.89@gmail.com>
2020-12-02 16:09:39 +0200

    redesigned home page

--- frontend/src/views/home.cmp.vue
+++ frontend/src/views/home.cmp.vue
@@ -1,35 +1,63 @@
 <template>
     <section class="content-wrapper main-layout">
-        <div class="main-hero flex">
+        <div class="main-hero flex full">
             <h1>Build a website easily!</h1>
+            <img
+                class="hero-img"
+                src="../assets/img/temp-hero.jpg"
+            />
         </div>
 
         <div class="home-card flex">
-            <div class="card-content">
+            <div class="card-content flex column space-between">
+                <span class="home-tag">Accessibility</span>
                 <h2>Build your own websites that you can be proud of!</h2>
                 <p>
                     Our platform will let you build you own websites using only
-                    your imagination.
+                    your imagination. Webcraft empowers you to to build
+                    professional, custom websites in a completely visual canvas.
                 </p>
-                <p>
-                    Webcraft empowers you to to build professional, custom
-                    websites in a completely visual canvas.
-                </p>
+                <button class="home-btn btn">start building</button>
             </div>
-            <img class="card-img" src="https://imagizer.imageshack.com/img924/8105/aLPK9v.png" />
+            <img
+                class="card-img"
+                src="https://imagizer.imageshack.com/img924/8105/aLPK9v.png"
+            />
         </div>
         <div class="home-card flex">
-            <div class="card-content">
-                <h2 class="title">Just Drag &amp; Drop</h2>
+            <div class="card-content flex column space-between">
+                 <span class="home-tag">Accessibility</span>
+                <h2 class="title">Just Drag &amp; Drop To Begin!</h2>
                 <p class="intro-text">
-                    With a super most innovative drag and drop website builder,
-                    you can design any website you want. Just Drag, Drop and
-                    Customize! All the templates below were built using Webcraft
-                    Platform.
+                    With a simple and drag and drop website builder, you can
+                    design any website you want. Just <span>Drag</span>,
+                    <span>Drop</span> and <span>Customise!</span> All the
+                    templates below were built using the Webcraft platform.
                 </p>
+                <button class="home-btn btn">start building</button>
             </div>
-            <img class="card-img" src="https://imagizer.imageshack.com/img922/6240/9IpCzn.png" />
+            <img
+                class="card-img"
+                src="https://imagizer.imageshack.com/img922/6240/9IpCzn.png"
+            />
             <!-- <img src="../assets/showtoasi/img5.svg" /> -->
+        </div>
+        <div class="home-card flex">
+            <div class="card-content flex column space-between">
+                 <span class="home-tag">Accessibility</span>
+                <h2 class="title">Just Drag &amp; Drop To Begin!</h2>
+                <p class="intro-text">
+                    With a simple and drag and drop website builder, you can
+                    design any website you want. Just <span>Drag</span>,
+                    <span>Drop</span> and <span>Customise!</span> All the
+                    templates below were built using the Webcraft platform.
+                </p>
+                <button class="home-btn btn">start building</button>
+            </div>
+            <img
+                class="card-img"
+                src="https://imagizer.imageshack.com/img922/6240/9IpCzn.png"
+            />
         </div>
 
         <loader />
