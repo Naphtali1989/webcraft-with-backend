c6f5389cb2c11dd1865520214d69225e0af792c0
Idanatiya <idanatiya122@gmail.com>
2020-12-03 17:15:52 +0200

    a

---
+++ frontend/src/cmps/samples/card-samples/music-card.cmp.vue
@@ -0,0 +1,176 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: 'section',
+            type: 'card',
+            class: 'main-layout',
+            style: {
+                borderRadius: '0px',
+                background: ''
+            },
+            children: [
+                {
+                    name: 'div',
+                    class: 'full',
+                    style: {
+                        borderRadius: '0px',
+                        background: '',
+                        padding: "0px"
+                    },
+                    children: [
+                        {
+                            name: 'div',
+                            style: {
+                                borderRadius: '0px',
+                                background: '',
+                                padding: "0px"
+                            },
+                            children: [
+                                {
+                                    name: 'img',
+                                    imgUrl: 'https://imagizer.imageshack.com/img923/510/eHFHGZ.jpg',
+                                    style: {
+                                        borderRadius: '0px',
+                                        maxWidth: "100%"
+
+                                    },
+                                }
+                            ]
+                        },
+                        {
+                            name: 'div',
+                            style: {
+                                borderRadius: '0px',
+                                background: '',
+                                padding: "0px"
+                            },
+                            children: [
+                                {
+                                    name: 'div',
+                                    style: {
+                                        background: '#ff1d00',
+                                        width: "100%",
+                                        height: '90%',
+                                        margin: '32px 0px'
+                                    },
+                                    children: [
+                                        {
+                                            name: 'div',
+                                            style: {
+                                                background: '',
+                                                paddingTop: '80px',
+                                                marginLeft: '-80px'
+                                            },
+                                            children: [
+                                                {
+                                                    name: 'img',
+                                                    class: 'flex align-center justify-center',
+                                                    imgUrl: 'https://imagizer.imageshack.com/img923/293/KH7i8X.png',
+                                                    style: {
+                                                        marginLeft: '250px'
+
+                                                    },
+                                                    children: [
+                                                        {
+                                                            name: 'txt',
+                                                            txt: "MEET OUR MUSIC STARS",
+                                                            style: {
+                                                                padding: '12px 0px',
+                                                                fontSize: "55px",
+                                                                letterSpacing: '1px',
+                                                                fontWeight: 'bold',
+                                                                background: '',
+                                                                border: 'none',
+                                                                "lineHeight": "60px",
+                                                                "textDecoration": "",
+                                                                "fontStyle": "",
+                                                                "textAlign": "",
+                                                                "fontFamily": "",
+                                                                "color": '#fff'
+                                                            }
+                                                        },
+                                                        {
+
+                                                            name: 'txt',
+                                                            txt: "It is a long established fact that a reader will be distracted by the readable",
+                                                            style: {
+
+                                                                padding: '12px 0px',
+                                                                fontSize: "17px",
+                                                                letterSpacing: '1px',
+                                                                fontWeight: 'bold',
+                                                                background: '',
+                                                                border: 'none',
+                                                                "lineHeight": "22px",
+                                                                "textDecoration": "",
+                                                                "fontStyle": "",
+                                                                "textAlign": "",
+                                                                "fontFamily": "",
+                                                                "color": '#fff'
+                                                            }
+                                                        },
+                                                        {
+
+                                                            name: 'txt',
+                                                            txt: "It is a long established fact that a reader will be distracted by the readable",
+                                                            style: {
+
+                                                                padding: '12px 0px',
+                                                                fontSize: "17px",
+                                                                letterSpacing: '1px',
+                                                                fontWeight: 'bold',
+                                                                background: '',
+                                                                border: 'none',
+                                                                "lineHeight": "22px",
+                                                                "textDecoration": "",
+                                                                "fontStyle": "",
+                                                                "textAlign": "",
+                                                                "fontFamily": "",
+                                                                "color": '#fff'
+                                                            }
+                                                        },
+                                                        {
+
+                                                            name: 'txt',
+                                                            txt: "It is a long established fact that a reader will be distracted by the readable",
+                                                            style: {
+                                                                padding: '12px 0px',
+                                                                fontSize: "17px",
+                                                                letterSpacing: '1px',
+                                                                fontWeight: 'bold',
+                                                                background: '',
+                                                                border: 'none',
+                                                                "lineHeight": "22px",
+                                                                "textDecoration": "",
+                                                                "fontStyle": "",
+                                                                "textAlign": "",
+                                                                "fontFamily": "",
+                                                                "color": '#fff'
+                                                            }
+                                                        },
+                                                    ]
+                                                }
+                                            ]
+
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+
+                    ]
+                }
+            ]
+
+        }
+    }
+}
+</script>
+
+<style>
+</style>
