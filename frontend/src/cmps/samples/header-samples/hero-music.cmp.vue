e4d7c3834029b9f5c38e642c42c8974c2c15896f
Idanatiya <idanatiya122@gmail.com>
2020-12-03 13:49:03 +0200

    a

---
+++ frontend/src/cmps/samples/header-samples/hero-music.cmp.vue
@@ -0,0 +1,152 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            "name": "section",
+            "type": "header",
+            "class": "main-layout hero-section flex",
+            "title": "hero with button",
+            "thumbnail": "https://imagizer.imageshack.com/img924/6563/GxmkNX.png",
+            "imgUrl": "https://imagizer.imageshack.com/img923/4010/YM2aNr.jpg",
+            "style": {
+                "background": "url(https://imagizer.imageshack.com/img923/4010/YM2aNr.jpg) center center / cover no-repeat",
+                "borderRadius": "0px",
+                "height": "100vh",
+                "width": "100%",
+                "padding": ""
+            },
+            "children": [
+                {
+                    "_id": "v5plz0hch2",
+                    "name": "div",
+                    "type": "inner-img-section",
+                    "class": "hero-section-img flex column justify-center align-center",
+                    "style": {
+                        "background": "rgba(0,0,0,70%)",
+                        "borderRadius": "0px",
+                        "height": "100%",
+                        "width": "100%"
+                    },
+                    "children": [
+                        {
+                            "_id": "7egll1rz1n",
+                            "name": "txt",
+                            "type": "inner-header",
+                            "class": "hero-inner-text",
+                            "txt": "MP3",
+                            "style": {
+
+                                "lineHeight": "90px",
+                                "letterSpacing": "2px",
+                                "background": "transparent",
+                                color: "#fff",
+                                "fontWeight": "bold",
+                                "fontFamily": "",
+                                "fontSize": "64px",
+                                "textDecoration": "inherit",
+                                "fontStyle": "inherit",
+                                "textAlign": "center",
+                                "borderRadius": "0px",
+                            }
+                        },
+                        {
+                            "_id": "6vc39dzgx9",
+                            "name": "txt",
+                            "type": "inner-text",
+                            "class": "hero-inner-text",
+                            "txt": "The Perfect Music",
+                            "style": {
+                                "padding": "0 20px",
+                                "lineHeight": 1.5,
+                                "letterSpacing": "2px",
+                                "background": "#f70606ab",
+                                "color": "#efefef",
+                                "fontWeight": normal,
+                                "fontFamily": "",
+                                "fontSize": "51px",
+                                "textDecoration": "inherit",
+                                "fontStyle": "inherit",
+                                "textAlign": "center",
+                                "borderRadius": "0px",
+                                "maxWidth": "62ch"
+                            }
+                        },
+                        {
+                            "_id": "7egll1rz1n",
+                            "name": "txt",
+                            "type": "inner-header",
+                            "class": "hero-inner-text",
+                            "txt": "Welcome To The Music",
+                            "style": {
+                                "lineHeight": "90px",
+                                "letterSpacing": "2px",
+                                "background": "transparent",
+                                color: "#fff",
+                                "fontWeight": "bold",
+                                "fontFamily": "",
+                                "fontSize": "64px",
+                                "textDecoration": "inherit",
+                                "fontStyle": "inherit",
+                                "textAlign": "center",
+                                "borderRadius": "0px",
+                            }
+                        },
+                        {
+                            "_id": "7egll1rz1n",
+                            "name": "txt",
+                            "type": "inner-header",
+                            "class": "hero-inner-text",
+                            "txt": "Lorem ipsum dolor sit amet consec tetur adipiscing elit elit at felis lacinia nec et estaptent taciti sociosqu ad litora torquent per conubia nostra",
+                            "style": {
+                                "padding": "0px 0px 40px 0px",
+                                "letterSpacing": "2px",
+                                "background": "transparent",
+                                color: "#fff",
+                                "fontWeight": "bold",
+                                "fontFamily": "",
+                                "fontSize": "17px",
+                                "textDecoration": "inherit",
+                                "fontStyle": "inherit",
+                                "textAlign": "center",
+                                "borderRadius": "0px",
+                            }
+                        },
+                        {
+
+                            "_id": "7egll1rz1n",
+                            "name": "txt",
+                            "type": "inner-header",
+                            "class": "hero-inner-text",
+                            "txt": "Lorem ipsum dolor sit amet consec tetur adipiscing elit elit at felis lacinia nec et estaptent taciti sociosqu ad litora torquent per conubia nostra",
+                            "style": {
+                                "padding": "12px 0",
+                                "border": "#fff solid 1px",
+                                "lineHeight": "30px",
+                                "width": "209px",
+                                "padding": "0px 0px 40px 0px",
+                                "letterSpacing": "2px",
+                                "background": "#fff",
+                                "color": "#d70a03",
+                                "fontWeight": "500",
+                                "fontFamily": "",
+                                "fontSize": "17px",
+                                "textDecoration": "inherit",
+                                "fontStyle": "inherit",
+                                "textAlign": "center",
+                                "borderRadius": "0px",
+                            }
+                        },
+                    ]
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+</style>
---
+++ frontend/src/cmps/samples/header-samples/navbar-food.cmp.vue
@@ -0,0 +1,180 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: 'section',
+            class: "main-layout",
+            type: 'header',
+            style: {
+                background: '#070100',
+                borderRadius: '0px',
+            },
+            children: [
+                {
+                    _id: 'kjasdjkwywvbvia',
+                    type: 'div',
+                    class: 'full',
+                    style: {
+                        background: '#070100',
+                        borderRadius: '0px',
+                        padding: '23px 0px',
+                        borderTop: '#cf2323 solid 5px',
+                        borderBottom: '#cf2323 solid 5px    
+                    },
+                    children: [
+                        {
+                            _id: 'vnsjsiulsdvavsw',
+                            type: 'div',
+                            class: 'flex space-between align-center',
+                            imgUrl: '',
+                            style: {
+                                borderRadius: '0px',
+                                background: ''
+                            },
+                            children: [
+                                {
+                                    name: 'img',
+                                    _id: 'lksdalksdwlaks',
+                                    imgUrl: 'https://imagizer.imageshack.com/img922/3100/nc0kKK.jpg',
+                                    style: {
+                                    }
+                                },
+                                {
+                                    name: 'div',
+                                    _id: 'jhaskdhjdsahjkwyw',
+                                    imgUrl: '',
+                                    style: {
+                                        borderRadius: '0px',
+                                        background: '',
+                                    },
+                                    children: [
+                                        {
+                                            id: "vnbvmsdhjsadhj",
+                                            name: 'txt',
+                                            txt: "HOME",
+                                            style: {
+                                                boxShadow: '#9d9d9d 0px 0px 56px 26px',
+                                                fontSize: "16px",
+                                                letterSpacing: '1px',
+                                                fontWeight: '500',
+                                                background: '#000',
+                                                border: 'none',
+                                                "color": '#fff',
+                                                "lineHeight": "",
+                                                "textDecoration": "",
+                                                "fontStyle": "",
+                                                "textAlign": "",
+                                                "fontFamily": "",
+                                            }
+                                        },
+                                        {
+                                            id: "vnbvmsdhjsadhj",
+                                            name: 'txt',
+                                            txt: "ABOUT",
+                                            style: {
+                                                fontSize: "16px",
+                                                letterSpacing: '1px',
+                                                fontWeight: 'bold',
+                                                background: '',
+                                                border: 'none',
+                                                "lineHeight": "",
+                                                "textDecoration": "",
+                                                "fontStyle": "",
+                                                "textAlign": "",
+                                                "fontFamily": "",
+                                                "color": '#fff',
+                                            }
+                                        },
+                                        {
+                                            id: "vnbvmsdhjsadhj",
+                                            name: 'ALBUMS',
+                                            txt: "It was a real expirience for me. The quality of food was only matched by the quality of service i got. I wish I can keep the flavor in my mouth for ever. Thanks for wonderful evening!",
+                                            style: {
+                                                fontSize: "1rem",
+                                                letterSpacing: '1px',
+                                                fontWeight: 'bold',
+                                                background: '',
+                                                border: 'none',
+                                                "lineHeight": "",
+                                                "textDecoration": "",
+                                                "fontStyle": "",
+                                                "textAlign": "",
+                                                "fontFamily": "",
+                                                "color": '#dbbe75'
+                                            }
+                                        },
+                                        {
+                                            id: "dshyaakldnvxe",
+                                            name: 'txt',
+                                            txt: "BLOG",
+                                            style: {
+                                                fontSize: "16px",
+                                                letterSpacing: '1px',
+                                                fontWeight: 'bold',
+                                                background: '',
+                                                border: 'none',
+                                                "lineHeight": "",
+                                                "textDecoration": "",
+                                                "fontStyle": "",
+                                                "textAlign": "",
+                                                "fontFamily": "",
+                                                "color": '#fff'
+                                            }
+                                        },
+                                        {
+                                            id: "dsladsloadsjvw",
+                                            name: 'txt',
+                                            txt: "CONTACT",
+                                            style: {
+                                                fontSize: "16px",
+                                                letterSpacing: '1px',
+                                                fontWeight: 'bold',
+                                                background: '',
+                                                border: 'none',
+                                                "lineHeight": "",
+                                                "textDecoration": "",
+                                                "fontStyle": "",
+                                                "textAlign": "",
+                                                "fontFamily": "",
+                                                "color": '#fff'
+                                            }
+                                        },
+                                        {
+                                            id: "ddasasyjfakla",
+                                            name: 'txt',
+                                            txt: "LOGIN",
+                                            style: {
+                                                fontSize: "16px",
+                                                letterSpacing: '1px',
+                                                fontWeight: 'bold',
+                                                background: '',
+                                                border: 'none',
+                                                "lineHeight": "",
+                                                "textDecoration": "",
+                                                "fontStyle": "",
+                                                "textAlign": "",
+                                                "fontFamily": "",
+                                                "color": '#fff'
+                                            }
+                                        }
+                                    ]
+
+                                }
+
+                            ]
+                        }
+                    ]
+
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+</style>
