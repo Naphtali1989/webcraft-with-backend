c76173c8088db78f32139fd38a6d7ced98512992
Lital Dincmen <litadi158@gmail.com>
2020-12-05 00:08:00 +0200

    woring on responsiveness

--- frontend/src/cmps/app-header.cmp.vue
+++ frontend/src/cmps/app-header.cmp.vue
@@ -1,82 +1,75 @@
 <template>
-    <section class="app-header flex space-between">
-        <div
-            class="logo place-center flex"
-            @click="pushHome"
-        >
-            <i class="fas fa-hammer fa-2x"></i>Web<span class="logo-span">craft</span>
-        </div>
-        <div class="nav-links flex align-center">
-            <router-link
-                to="/"
-                exact
-            >Home</router-link>
-            <router-link
-                to="/wap"
-                exact
-            >Templates</router-link>
-            <router-link
-                to="/editor"
-                exact
-            >Editor</router-link>
-            <!-- <router-link to="/playground">Playground</router-link> -->
-            <a
-                @click="showSignup"
-                class="cursor"
-                v-if="!loggedInUser"
-            >Login</a>
-            <avatar
-                :username="loggedInUser.username"
-                :imgUrl="loggedInUser.imgUrl"
-                :size="40"
-                @click.native="toggleModal"
-                :color="'#124a76'"
-                v-if="loggedInUser"
-            />
-
-        </div>
-        <avatar-modal
-            v-if="showModal"
-            :loggedInUser="loggedInUser"
-            @closeModal="showModal = false"
-        />
-    </section>
+  <section class="app-header flex space-between">
+    <div class="logo place-center flex" @click="pushHome">
+      <i class="fas fa-hammer fa-2x"></i>Web<span class="logo-span">craft</span>
+    </div>
+    <nav class="nav-links flex align-center">
+      <router-link to="/" exact>Home</router-link>
+      <router-link to="/wap" exact>Templates</router-link>
+      <router-link to="/editor" exact>Editor</router-link>
+      <a @click="showSignup" class="cursor" v-if="!loggedInUser">Login</a>
+      <avatar
+        :username="loggedInUser.username"
+        :imgUrl="loggedInUser.imgUrl"
+        :size="40"
+        @click.native="toggleModal"
+        :color="'#124a76'"
+        v-if="loggedInUser"
+      />
+    </nav>
+    <div class="hamburger flex-center column btn" @click="toggleMenu">
+      <div class="bar1"></div>
+      <div class="bar2"></div>
+      <div class="bar3"></div>
+    </div>
+    <avatar-modal
+      v-if="showModal"
+      :loggedInUser="loggedInUser"
+      @closeModal="showModal = false"
+    />
+  </section>
 </template>
 
 <script>
 import avatar from '@/cmps/avatar.cmp.vue';
 import avatarModal from '@/cmps/avatar-modal.cmp.vue';
 export default {
-    name: "app-header",
-    data() {
-        return {
-            showModal: false
-        }
+  name: "app-header",
+  data() {
+    return {
+      showModal: false
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
     },
-    methods: {
-        toggleModal() {
-            this.showModal=!this.showModal;
-        },
-        pushHome() {
-            this.$router.push('/')
-        },
-        showSignup() {
-            this.$emit('openSignupModal')
-        },
-
+    pushHome() {
+      this.$router.push('/')
     },
-    computed: {
-        loggedInUser() {
-            return this.$store.getters.loggedInUser;
-        },
-        // avatar() {
-        //     if(this.loggedInUser&&this.loggedInUser.imgUrl) return this.loggedInUser.imgUrl
-        //     return this.loggedInUser.username
-        // }
+    showSignup() {
+      this.$emit('openSignupModal')
     },
-    components: {
-        avatar,
-        avatarModal
+    toggleMenu() {
+      console.log('Am i here?')
+      document.body.classList.toggle('open-menu');
+    }
+  },
+  computed: {
+    loggedInUser() {
+      return this.$store.getters.loggedInUser;
     },
+    // avatar() {
+    //     if(this.loggedInUser&&this.loggedInUser.imgUrl) return this.loggedInUser.imgUrl
+    //     return this.loggedInUser.username
+    // }
+  },
+  destroyed() {
+    document.body.classList.remove('open-menu');
+  },
+  components: {
+    avatar,
+    avatarModal
+  },
 };
 </script>
