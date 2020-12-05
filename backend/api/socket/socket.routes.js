cfd6e96976ff41fd667affd012e6d46bf8b14493
Idanatiya <idanatiya122@gmail.com>
2020-12-05 04:24:03 +0200

    a

--- backend/api/socket/socket.routes.js
+++ backend/api/socket/socket.routes.js
@@ -9,37 +9,5 @@ function connectSockets(io) {
             console.log('getting wap?:', wap);
             socket.broadcast.emit('savedWap', wap)
         })
-
-        //     socket.on('chat newMsg', msg => {
-        //         console.log('msg has been recived: ', msg)
-        //         console.log('socket toy id:', socket.toyId);
-        //         //send the msgs to all boradcasts expect the sender
-        //         socket.broadcast.emit('chat addMsg', msg)
-
-        //     })
-
-        //     socket.on('set-typing', username => {
-        //         console.log('who is typing:', username);
-        //         socket.broadcast.emit('set-typing', username)
-        //         socket.broadcast.emit('rest-typing', true)
-        //     })
-        //     socket.on('rest-typing', () => {
-        //         socket.broadcast.emit('rest-typing', false)
-        //     })
-
-        //     socket.on('delete-toy', (toyId) => {
-        //         //ajax request to the routes
-        //         socket.emit('delete-toy', toyId)
-        //     })
-
-        //     socket.on('chat-topic', toyId => {
-        //         console.log('*****topic*****:', toyId);
-        //         if (socket.toyId) {
-        //             socket.leave(socket.toyId)
-        //         }
-        //         socket.join(toyId)
-        //         socket.toyId = toyId;
-        //     })
-        // })
     })
 }
