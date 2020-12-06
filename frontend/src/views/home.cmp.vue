adbd899b8db0a4eea92d5b681a95fdd001fa2899
Naphtali Rubin <naphtali.r.89@gmail.com>
2020-12-06 20:55:55 +0200

    trying to redesign homepage

--- frontend/src/views/home.cmp.vue
+++ frontend/src/views/home.cmp.vue
@@ -1,31 +1,32 @@
 <template>
     <section class="content-wrapper main-layout">
         <div class="main-hero flex column full">
-            <h1>Build a website easily!</h1>
             <div class="hero-screen"></div>
             <img
                 class="hero-img"
                 src="../assets/webcraft-svg/picture-editor.svg"
             />
-            <button
-                class="home-btn hero-btn btn"
-                @click="pushEditor"
-            >Start Building</button>
+            <div class="home-btn hero-btn from-top btn" @click="pushEditor">
+                Start Building
+            </div>
         </div>
-
+        <h1>Build a website easily!</h1>
         <div class="home-card flex column">
             <div class="card-content flex column space-between">
                 <span class="home-tag text-left">Imagination</span>
-                <h2 class="title">Build your own websites that you can be proud of!</h2>
+                <h2 class="title">
+                    Build your own websites that you can be proud of!
+                </h2>
                 <p>
                     Our platform will let you build you own websites using only
-                    your imagination. <span class="logo-name">Webcraft</span> empowers you to to build
-                    professional, custom websites in a completely visual canvas.
+                    your imagination.
+                    <span class="logo-name">Webcraft</span> empowers you to to
+                    build professional, custom websites in a completely visual
+                    canvas.
                 </p>
-                <button
-                    class="home-btn btn"
-                    @click="pushEditor"
-                >Lets Create</button>
+                <button class="home-btn btn" @click="pushEditor">
+                    Let's Create!
+                </button>
             </div>
             <img
                 class="card-img"
@@ -37,15 +38,15 @@
                 <span class="home-tag text-right">Accessibility</span>
                 <h2 class="title">Suited for all your needs</h2>
                 <p class="intro-text">
-                    Suited for any of your needs
-                    Design and build your own high-quality websites. Whether you’re promoting your business, showcasing your work,
-                    opening your store or starting a blog — you can do it all with the <span class="logo-name">Webcraft</span> website editor.
-
+                    Suited for any of your needs Design and build your own
+                    high-quality websites. Whether you’re promoting your
+                    business, showcasing your work, opening your store or
+                    starting a blog — you can do it all with the
+                    <span class="logo-name">Webcraft</span> website editor.
                 </p>
-                <button
-                    class="home-btn btn"
-                    @click="pushTemplate"
-                >Check Templates</button>
+                <button class="home-btn btn" @click="pushTemplate">
+                    Check Templates
+                </button>
             </div>
             <img
                 class="card-img"
@@ -60,12 +61,12 @@
                     With a simple and drag and drop website builder, you can
                     design any website you want. Just <span>Drag</span>,
                     <span>Drop</span> and <span>Customise!</span> All the
-                    templates below were built using the <span class="logo-name">Webcraft</span> platform.
+                    templates below were built using the
+                    <span class="logo-name">Webcraft</span> platform.
                 </p>
-                <button
-                    class="home-btn btn"
-                    @click="pushEditor"
-                >Give It A Try</button>
+                <button class="home-btn btn" @click="pushEditor">
+                    Give it a try!
+                </button>
             </div>
 
             <img
