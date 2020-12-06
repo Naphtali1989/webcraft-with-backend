d3df2006dfaf22fa00f3f5f732700a833ca04e98
Matan Cohen <matanco3605@gmail.com>
2020-12-06 19:31:48 +0200

    wap gallery looking better

--- frontend/src/cmps/wap/wap-preview.cmp.vue
+++ frontend/src/cmps/wap/wap-preview.cmp.vue
@@ -3,7 +3,7 @@
     <div class="wap-preview text-center" >
         <h3 class="wap-title">{{ wap.title }}</h3>
         <img :src="wap.thumbnail" />
-        <div class="wap-prev-controls flex align-center space-around">
+        <div class="wap-prev-controls flex-center">
             <router-link :to="`/wap/${wap._id}`" title="View Template"><i class="fas fa-eye"></i></router-link>
             <router-link :to="`/editor/${wap._id}`" title="Edit Template"><i class="fas fa-pen"></i></router-link>
         </div>
